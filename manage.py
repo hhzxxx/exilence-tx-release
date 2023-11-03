7b4749bf33a2ade6f8dd5394aca65b1c9cd16b64
root <root@ubuntu2004>
2023-11-03 07:08:32 +0000

    Added manage.py

---
+++ manage.py
@@ -0,0 +1,16 @@
+import json
+
+# 打开 JSON 文件并加载数据
+with open('price.json', 'r') as json_file:
+    data = json.load(json_file)
+
+# 初始化总和变量
+total_count = 0
+
+# 遍历列表中的对象并计算 count 总和
+for item in data:
+    if 'count' in item:  # 检查对象中是否有 count 字段
+        total_count += item['count']
+
+# 打印 count 总和
+print(f'Total Count: {total_count}')
